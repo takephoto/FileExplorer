6351df324b8c434d6ad381adeb1f72bf7fdee32c
yang <yang92139@163.com>
2016-12-10 19:14:38 +0800

    新建项目

---
+++ FileExplore/app/src/main/java/com/example/yangyang/fileexplore/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.yangyang.fileexplore;
+
+import android.database.DataSetObserver;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView mListView ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mListView = (ListView) findViewById(R.id.file_list) ;
+        mListView.setAdapter(null);
+
+        
+    }
+    
+}
