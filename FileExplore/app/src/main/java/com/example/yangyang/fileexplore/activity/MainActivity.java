e4759b71612a05b0490418e8c65588ec152f4c7c
yang <yang92139@163.com>
2016-12-10 20:07:22 +0800

    Base

--- FileExplore/app/src/main/java/com/example/yangyang/fileexplore/activity/MainActivity.java
+++ FileExplore/app/src/main/java/com/example/yangyang/fileexplore/activity/MainActivity.java
@@ -22,8 +22,6 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
         mListView = (ListView) findViewById(R.id.file_list) ;
         mListView.setAdapter(null);
-
-        
     }
     
 }
